3fb12990182b9d745b0d41db439cf410e4218974
Povilas Staskus <povilas.staskus@gmail.com>
2016-01-15 14:48:17 +0200

    changecolor random

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -36,6 +36,6 @@ public class PlayerMovement : NetworkBehaviour {
 	[Command]
 	void CmdChangeColor() {
 		
-		GetComponent<SpriteRenderer> ().color = new Color (Random.Range (0.5f, 0.8f), Random.Range (0.5f, 0.8f), Random.Range (0.5f, 0.8f));
+		GetComponent<SpriteRenderer> ().color = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
 	}
 }
